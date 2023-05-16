9a6a77c1b94f91b4495365369a55882c34ba1c02
Phillip Sussman <phillipsussman@gmail.com>
2023-05-16 15:17:20 -0700

    refactored front-end code, added patch element to complete CRUD

--- public/app.js
+++ public/app.js
@@ -1,6 +1,5 @@
 const taskContainer = document.querySelector(".tasks");
 const taskInput = document.getElementById("description");
-const taskPriority = document.getElementById("priority");
 
 taskInput.focus();
 
@@ -23,84 +22,139 @@ form.addEventListener("submit", (event) => {
 	form.reset();
 });
 
+let taskCreation = (task) => {
+	// Tasks Div
+	const taskDiv = document.createElement("div");
+	taskDiv.className = "task-items";
+
+	//creating an H2 title for tasks
+	const h2 = document.createElement("h2");
+	h2.className = "task-name";
+	h2.textContent = task.description;
+
+	h2.addEventListener("click", () => {
+		const input = document.createElement("input");
+		input.setAttribute("type", "text");
+		input.setAttribute("value", h2.innerText);
+
+		input.addEventListener("blur", () => {
+			const newVal = input.value;
+			const taskID = task.id;
+
+			if (newVal === h2.innerHTML) {
+				console.log("nothing changed!");
+				input.replaceWith(h2);
+				return;
+			}
+			// PATCH REQUEST
+			fetch(`/api/tasks/${taskID}`, {
+				method: "PATCH",
+				body: JSON.stringify({ description: newVal }),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					console.log(data);
+					h2.innerText = data.description;
+					input.replaceWith(h2);
+				})
+				.catch((error) => {
+					console.error("Error updating text:", error);
+				});
+		});
+		h2.replaceWith(input);
+		input.focus();
+	});
+
+	//appending H2 to task DIV
+	taskDiv.append(h2);
+
+	// Priority info
+	const priorityContainer = document.createElement("span");
+	const priorityLabel = document.createElement("span");
+	const priorityNum = document.createElement("span");
+	priorityNum.className = "priority-num";
+	priorityLabel.textContent = "Priority: ";
+	priorityNum.textContent = task.priority;
+
+	priorityNum.addEventListener("click", () => {
+		// const input = document.createElement("input");
+		// input.setAttribute("type", "text");
+		// input.setAttribute("value", priorityNum.innerText);
+
+		const select = document.createElement("select");
+
+		for (let i = 1; i <= 3; i++) {
+			const option = document.createElement("option");
+			option.setAttribute("value", i);
+			option.innerText = i;
+			select.appendChild(option);
+		}
+
+		select.value = priorityNum.innerText;
+
+		select.addEventListener("blur", () => {
+			const newVal = select.value;
+			const taskID = task.id;
+
+			if (newVal === priorityNum.innerHTML) {
+				console.log("nothing changed!");
+				select.replaceWith(priorityNum);
+				return;
+			}
+			// PATCH REQUEST
+			fetch(`/api/tasks/${taskID}`, {
+				method: "PATCH",
+				body: JSON.stringify({ priority: newVal }),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					console.log(data);
+					priorityNum.innerText = data.priority;
+					select.replaceWith(priorityNum);
+				})
+				.catch((error) => {
+					console.error("Error updating text:", error);
+				});
+		});
+		priorityNum.replaceWith(select);
+		select.focus();
+	});
+
+	priorityContainer.append(priorityLabel, priorityNum);
+	taskDiv.append(priorityContainer);
+
+	// Delete Btn
+	const deleteBtn = document.createElement("button");
+	deleteBtn.className = "delete-btn";
+	deleteBtn.textContent = "Delete task!";
+	const btnID = task.id;
+
+	deleteBtn.addEventListener("click", () => {
+		// Fetching the DELETE REQUEST
+		fetch(`/api/tasks/${btnID}`, { method: "DELETE" });
+		console.log(btnID);
+	});
+	deleteBtn.addEventListener("click", () => {
+		taskDiv.remove();
+	});
+	// adds the delete btn to task div
+	taskDiv.append(deleteBtn);
+	// add the tasks to task container.
+	taskContainer.appendChild(taskDiv);
+};
+
 // GET REQUEST
 fetch("/api/tasks")
 	.then((res) => res.json())
 	.then((tasks) => {
 		for (let task of tasks) {
-			// Tasks Div
-			const taskDiv = document.createElement("div");
-			taskDiv.className = "task-items";
-
-			//creating an H2 title for tasks
-			const h2 = document.createElement("h2");
-			h2.className = "task-name";
-			h2.textContent = task.description;
-
-			h2.addEventListener("click", () => {
-				const input = document.createElement("input");
-				input.setAttribute("type", "text");
-				input.setAttribute("value", h2.innerText);
-
-				input.addEventListener("blur", () => {
-					const newVal = input.value;
-					const taskID = task.id;
-
-					if (newVal === h2.innerHTML) {
-						console.log("nothing changed!");
-						input.replaceWith(h2);
-						return;
-					}
-
-					fetch(`/api/tasks/${taskID}`, {
-						method: "PATCH",
-						body: JSON.stringify({ description: newVal }),
-						headers: {
-							"Content-Type": "application/json",
-						},
-					})
-						.then((res) => res.json())
-						.then((data) => {
-							console.log(data);
-							h2.innerText = data.description;
-							input.replaceWith(h2);
-						})
-						.catch((error) => {
-							console.error("Error updating text:", error);
-						});
-				});
-				h2.replaceWith(input);
-				input.focus();
-			});
-
-			//appending H2 to task DIV
-			taskDiv.append(h2);
-
-			// Priority info
-			const priorityNum = document.createElement("div");
-			priorityNum.className = "priority-num";
-			priorityNum.textContent = `Priority: ${task.priority}`;
-			taskDiv.append(priorityNum);
-
-			// Delete Btn
-			const deleteBtn = document.createElement("button");
-			deleteBtn.className = "delete-btn";
-			deleteBtn.textContent = "Delete task!";
-			const btnID = task.id;
-
-			deleteBtn.addEventListener("click", () => {
-				// Fetching the DELETE REQUEST
-				fetch(`/api/tasks/${btnID}`, { method: "DELETE" });
-				console.log(btnID);
-			});
-			deleteBtn.addEventListener("click", () => {
-				taskDiv.remove();
-			});
-			// adds the delete btn to task div
-			taskDiv.append(deleteBtn);
-
-			// add the tasks to task container.
-			taskContainer.appendChild(taskDiv);
+			taskCreation(task);
 		}
 	});
 
@@ -115,41 +169,7 @@ function postData(data) {
 	})
 		.then((response) => response.json())
 		.then((data) => {
-			console.log("Success:", data);
-			console.log(data[0].description);
-
-			// Tasks Div
-			const taskDiv = document.createElement("div");
-			taskDiv.className = "task-items";
-			const h2 = document.createElement("h2");
-			h2.textContent = data[0].description;
-			h2.className = "task-name";
-			taskDiv.append(h2);
-
-			// Priority info
-			const priorityNum = document.createElement("div");
-			priorityNum.className = "priority-num";
-			priorityNum.textContent = `Priority: ${data[0].priority}`;
-			taskDiv.append(priorityNum);
-
-			// Delete Btn
-			const deleteBtn = document.createElement("button");
-			deleteBtn.className = "delete-btn";
-			deleteBtn.textContent = "Delete task!";
-			const btnID = data[0].id;
-
-			deleteBtn.addEventListener("click", () => {
-				fetch(`/api/tasks/${btnID}`, { method: "DELETE" });
-				console.log(btnID);
-			});
-			deleteBtn.addEventListener("click", () => {
-				taskDiv.remove();
-			});
-			// adds the delete btn to task div
-			taskDiv.append(deleteBtn);
-
-			// add the task item div to task container.
-			taskContainer.appendChild(taskDiv);
+			taskCreation(data);
 		})
 		.catch((error) => {
 			console.error("Error:", error);
